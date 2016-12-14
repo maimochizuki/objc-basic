754eea8932d31341c3153b586cde6e2375597516
ITユーザー <it-user@kpc-it3146mc.kmt.neec.ac.jp>
2016-12-14 13:05:52 +0900

    @maimochizuki #2 着手

---
+++ ex1-1-2/ex1-1-2/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  ex1-1-2
+//
+//  Created by ITユーザー on 2016/12/14.
+//  Copyright © 2016年 ITユーザー. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    NSArray *niku = [NSArray arrayWithObjects:@"豚肉", @"牛肉", @"鶏肉", nil];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
