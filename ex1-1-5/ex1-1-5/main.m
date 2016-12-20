5d690102019b4180079c6d14b5e8b69274981a07
ITユーザー <it-user@kpc-it3146mc.kmt.neec.ac.jp>
2016-12-20 13:17:21 +0900

    #5 start

---
+++ ex1-1-5/ex1-1-5/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ex1-1-5
+//
+//  Created by ITユーザー on 2016/12/20.
+//  Copyright © 2016年 ITユーザー. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
