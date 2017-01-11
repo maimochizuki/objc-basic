8243680fbcba6dd5488cd1314d8c4906e0f631a2
ITユーザー <it-user@kpc-it3146mc.kmt.neec.ac.jp>
2017-01-11 11:44:33 +0900

    Fixed. #6 の課題が完了しました。

---
+++ ex1-1-6/ex1-1-6/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ex1-1-6
+//
+//  Created by ITユーザー on 2017/01/11.
+//  Copyright © 2017年 ITユーザー. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
